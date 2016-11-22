b643f61f21c5c386cf2b96d5ee51214e9a988a8d
Leviathan11 <gubik.levente@gmail.com>
2016-11-22 10:43:35 +0100

    ProductCategoryDaoJdbc implemented

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -2,18 +2,25 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.DatabaseConnection;
+import com.codecool.shop.model.ProductCategory;
 
-/**
- * Created by cave on 2016.11.22..
- */
-public abstract class ProductCategoryDaoJdbc implements ProductCategoryDao {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProductCategoryDaoJdbc implements ProductCategoryDao {
+
+    private static List<ProductCategory> DATA = new ArrayList<>();
     private static ProductCategoryDaoJdbc instance;
-
     private static DatabaseConnection databaseConnection;
 
     public static ProductCategoryDaoJdbc getInstance() {
         if (instance == null) {
-            instance = ProductCategoryDaoJdbc.getInstance();
+            instance = new ProductCategoryDaoJdbc();
             databaseConnection = DatabaseConnection.getInstance();
         }
         return instance;
@@ -21,12 +28,48 @@ public abstract class ProductCategoryDaoJdbc implements ProductCategoryDao {
 
     public static ProductCategoryDaoJdbc getInstance(DatabaseConnection dbConnection) {
         if (instance == null) {
-            instance = ProductCategoryDaoJdbc.getInstance();
+            instance = new ProductCategoryDaoJdbc();
             databaseConnection = dbConnection;
         }
         return instance;
     }
 
     private ProductCategoryDaoJdbc() {
+        String query = "SELECT * FROM product_categories;";
+        try (Connection connection = databaseConnection.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                ProductCategory actCategory = new ProductCategory(resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getString(resultSet.getString("department")));
+                DATA.add(actCategory);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void add(ProductCategory category) {
+        category.setId(DATA.size() + 1);
+        DATA.add(category);
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return DATA.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public void remove(int id) {
+        DATA.remove(find(id));
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return DATA;
     }
 }
--- src/main/java/com/codecool/shop/model/DatabaseConnection.java
+++ src/main/java/com/codecool/shop/model/DatabaseConnection.java
@@ -4,13 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Created by cave on 2016.11.21..
- */
 public class DatabaseConnection {
     private static final String DATABASE = "jdbc:postgresql://localhost:5432/codecoolshop";
     private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "postgres";
+    private static final String DB_PASSWORD = "123glevi02";
 
     private static DatabaseConnection dbConnection = new DatabaseConnection();
 
