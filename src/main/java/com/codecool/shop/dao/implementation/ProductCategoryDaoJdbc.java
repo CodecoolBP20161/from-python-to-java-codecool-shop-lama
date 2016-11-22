f34ca2af3fcce12e9319fe0f53762aa65e2cb7d1
Ádám Horváth <horada1991@gmail.com>
2016-11-22 10:32:54 +0100

    hotfix in DaoJdbc constructors

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -2,18 +2,21 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.DatabaseConnection;
+import com.codecool.shop.model.ProductCategory;
+
+import java.util.List;
 
 /**
  * Created by cave on 2016.11.22..
  */
-public abstract class ProductCategoryDaoJdbc implements ProductCategoryDao {
+public class ProductCategoryDaoJdbc implements ProductCategoryDao {
     private static ProductCategoryDaoJdbc instance;
 
     private static DatabaseConnection databaseConnection;
 
     public static ProductCategoryDaoJdbc getInstance() {
         if (instance == null) {
-            instance = ProductCategoryDaoJdbc.getInstance();
+            instance = new ProductCategoryDaoJdbc();
             databaseConnection = DatabaseConnection.getInstance();
         }
         return instance;
@@ -21,12 +24,32 @@ public abstract class ProductCategoryDaoJdbc implements ProductCategoryDao {
 
     public static ProductCategoryDaoJdbc getInstance(DatabaseConnection dbConnection) {
         if (instance == null) {
-            instance = ProductCategoryDaoJdbc.getInstance();
+            instance = new ProductCategoryDaoJdbc();
             databaseConnection = dbConnection;
         }
         return instance;
     }
 
     private ProductCategoryDaoJdbc() {
+    }
+
+    @Override
+    public void add(ProductCategory category) {
+
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return null;
     }
 }
