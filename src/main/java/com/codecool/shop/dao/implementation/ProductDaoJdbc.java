4e291018480b42befbcc6ca1e40603463cb3eb13
Leviathan11 <gubik.levente@gmail.com>
2016-11-23 11:25:10 +0100

    testing removed

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -175,44 +175,4 @@ public class ProductDaoJdbc implements ProductDao {
         product.setId(result.getInt("id"));
         return product;
     }
-
-    public static void main(String[] args) {
-        ProductDao productDataStore = getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJdbc.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoJdbc.getInstance();
-        try {
-            System.out.println(productDataStore.getBy(productCategoryDataStore.find(2)));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-//        //setting up a new supplier
-//        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-//        supplierDataStore.add(amazon);
-//        Supplier lenovo = new Supplier("Lenovo", "Computers");
-//        supplierDataStore.add(lenovo);
-//
-//        //setting up a new product category
-//        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-//        productCategoryDataStore.add(tablet);
-//        ProductCategory phone = new ProductCategory("Phone", "Hardware", "phone");
-//        productCategoryDataStore.add(phone);
-//        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "laptop");
-//        productCategoryDataStore.add(laptop);
-//        ProductCategory headset = new ProductCategory("Headset", "Hardware", "headset");
-//        productCategoryDataStore.add(headset);
-//
-//
-//        //setting up products and printing it
-//        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-//        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-//        productDataStore.add(new Product("Lenovo 5520", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", phone, amazon));
-//        productDataStore.add(new Product("Sony Headset Stuff", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", headset, amazon));
-//        productDataStore.add(new Product("Apple Ipad", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-//        productDataStore.add(new Product("HP ProBook", 89, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", laptop, lenovo));
-//        productDataStore.add(new Product("Nokia 2110", 89,  "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", phone, amazon));
-//        productDataStore.add(new Product("Asus ZenBook", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", laptop, lenovo));
-    }
-
-
 }
