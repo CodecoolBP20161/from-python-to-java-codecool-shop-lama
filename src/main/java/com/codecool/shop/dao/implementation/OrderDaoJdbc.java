9c9563806fb76ea01ded487f28a60db3673b629e
annakertesz <kerteszannanak@gmail.com>
2016-12-06 14:26:59 +0100

    OrderDao Interface with add method

---
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoJdbc.java
@@ -0,0 +1,14 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.order.implementation.Order;
+
+/**
+ * Created by annakertesz on 12/6/16.
+ */
+public class OrderDaoJdbc implements OrderDao{
+    @Override
+    public void add(Order order) {
+
+    }
+}
