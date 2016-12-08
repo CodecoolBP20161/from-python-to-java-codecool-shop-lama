6bac3575b8bf9c3632dd6f28a686193b4a02c8e1
robotkutya <mihalyfuredi@gmail.com>
2016-12-08 12:42:25 +0100

    SMTP settings and SendEmail prototype

---
+++ src/main/java/com/codecool/shop/email/SendEmail.java
@@ -0,0 +1,63 @@
+package com.codecool.shop.email;
+
+import com.codecool.shop.model.customer.Customer;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Created by prezi on 2016. 12. 08..
+ */
+public class SendEmail {
+    private static SMTPConnection smtp;
+    private static SendEmail INSTANCE;
+
+    public static SendEmail getInstance(){
+        if (INSTANCE == null) {
+            INSTANCE = new SendEmail();
+            smtp = SMTPConnection.getInstance();
+        }
+        return INSTANCE;
+    }
+
+    private Session makeSession(){
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", smtp.getHost());
+        props.put("mail.smtp.port", smtp.getPort());
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(smtp.getUser(), smtp.getPassword());
+                    }
+                });
+        return session;
+    }
+
+    public void sendRegistrationEmail(Customer customer){
+        Session session = makeSession();
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(smtp.getUser()));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(customer.getEmail()));
+            message.setSubject("Registration");
+            message.setText("Dear " + customer.getName() + ","
+                    + "\n\n Welcome to our shop."
+                    + customer.toString());
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
